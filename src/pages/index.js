dbfd9b59c141ca2dab8757c82c79afd20420d052
sadie <sgill@flowwest.com>
2020-07-01 15:58:35 -0700

    adds klamath logo to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,29 @@ import { Row, Col, Container, ListGroup } from "react-bootstrap"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Img from "gatsby-image"
+import { graphql } from "gatsby"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout pageInfo={{ pageName: "index" }}>
     <SEO title="Home" keywords={[`gatsby`, `react`, `bootstrap`]} />
     <Container className="text-center">
-     <h1>Hi</h1>
+      <Img fluid={data.file.childImageSharp.fluid} />
+      <h1>Hi</h1>
     </Container>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "klamathtribes1200.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
