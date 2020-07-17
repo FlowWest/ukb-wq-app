9a4c8a231fe0347aacae9a15bad40b0d143b9e0b
sadie <sgill@flowwest.com>
2020-07-17 11:48:32 -0700

    adds dataset download component

--- src/pages/data-repository.js
+++ src/pages/data-repository.js
@@ -3,12 +3,14 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import DataDownload from "../components/dataDownload"
 
 const DataRepo = () => (
   <Layout pageInfo={{ pageName: "data-repository" }}>
     <SEO title="Data Repository" />
     <h1>Data Repository</h1>
     <p>Downloads available</p>
+    <DataDownload/>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
