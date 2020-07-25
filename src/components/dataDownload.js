67ba37fdf9868122f63413dc2f5c7b8aee4207bb
sadie <sgill@flowwest.com>
2020-07-24 17:01:44 -0700

    puts place holder text in for download data objects

--- src/components/dataDownload.js
+++ src/components/dataDownload.js
@@ -1,12 +1,18 @@
 import React from "react"
 import { Card, DropdownButton, Dropdown } from "react-bootstrap"
 
-const DataDownload = () => {
+const DataDownload = props => {
   return (
     <Card>
-      <Card.Header as="h5">Dataset Name</Card.Header>
+      <Card.Header as="h5">{props.datasetName}</Card.Header>
       <Card.Body>
-        <Card.Text>Parameter descriptions</Card.Text>
+        <Card.Text>Geographic Extent: {props.geographicExtent}</Card.Text>
+        <Card.Text>
+          Temporal Coverage: {props.startYear}-{props.endYear}
+        </Card.Text>
+        <Card.Text>
+          Analytes Description: {props.description}
+        </Card.Text>
         <DropdownButton id="dropdown-basic-button" title="Download">
           <Dropdown.Item href="#">csv</Dropdown.Item>
           <Dropdown.Item href="#">excel</Dropdown.Item>
