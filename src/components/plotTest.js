0eca8117b469589a5c48119c2f250e36dd74b789
sadie <sgill@flowwest.com>
2020-12-16 10:39:21 -0800

    adds example plotly plot to dashboard

--- src/components/plotTest.js
+++ src/components/plotTest.js
@@ -1,15 +1,43 @@
-`query {
-  allRiverCsv(filter: {Characteristic_Name: {eq: "pH"}}) {
-    edges {
-      node {
-        id
-        Activity_Start_Date
-        Monitoring_Location_ID
-        Characteristic_Name
-        Result_Value
-        Result_Unit
-        Result_Detection_Condition
+import React from "react"
+import Plot from "react-plotly.js"
+import { useStaticQuery, graphql } from "gatsby"
+
+const WQPlot = () => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        allRiverCsv(filter: { Characteristic_Name: { eq: "pH" } }) {
+          edges {
+            node {
+              id
+              Activity_Start_Date
+              Monitoring_Location_ID
+              Characteristic_Name
+              Result_Value
+              Result_Unit
+              Result_Detection_Condition
+            }
+          }
+        }
       }
-    }
-  }
-}`
+    `
+  )
+  const dates = data.allRiverCsv.edges.map(edge => edge.node.Activity_Start_Date)
+  const values = data.allRiverCsv.edges.map(edge => edge.node.Result_Value)
+
+  return (
+    <Plot
+      data={[
+        {
+          x: dates,
+          y: values,
+          type: 'scatter',
+          mode: 'lines+markers',
+          marker: {color: 'green'}
+        }
+      ]}
+    />
+  )
+}
+
+export default WQPlot
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,12 +3,14 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import WQPlot from "../components/plotTest"
 
 const Dashboard = () => (
   <Layout pageInfo={{ pageName: "dashboard" }}>
     <SEO title="Dashboard" />
     <h1>Water Quality Dashboard</h1>
     <p>data stuff</p>
+    <WQPlot />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
