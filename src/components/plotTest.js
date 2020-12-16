1e2ea5c594c14072e06ca43880d4c3643151f5e4
sadie <sgill@flowwest.com>
2020-12-16 13:03:35 -0800

    adds dropdown for analytes

--- src/components/plotTest.js
+++ src/components/plotTest.js
@@ -1,12 +1,15 @@
 import React from "react"
 import Plot from "react-plotly.js"
 import { useStaticQuery, graphql } from "gatsby"
+import { Dropdown } from "semantic-ui-react"
 
 const WQPlot = () => {
+  const characteristicName = "pH" // TODO move to state
+
   const data = useStaticQuery(
     graphql`
       query {
-        allRiverCsv(filter: { Characteristic_Name: { eq: "pH" } }) {
+        results: allRiverCsv {
           edges {
             node {
               id
@@ -19,24 +22,58 @@ const WQPlot = () => {
             }
           }
         }
+        characteristicOptions: allRiverCsv {
+          distinct(field: Characteristic_Name)
+        }
       }
     `
   )
-  const dates = data.allRiverCsv.edges.map(edge => edge.node.Activity_Start_Date)
-  const values = data.allRiverCsv.edges.map(edge => edge.node.Result_Value)
+  
+  const characteristicNames = data.characteristicOptions.distinct
+  
+  const characteristicOptions = characteristicNames.map(
+    characteristic => {
+      let option = {}
+      option.key = characteristic
+      option.value = characteristic
+      option.text = characteristic
+      return option
+    }
+  )
+
+  const filteredData = data.results.edges.filter(
+    edge => edge.node.Characteristic_Name === characteristicName
+  )
+
+  const dates = filteredData.map(edge => edge.node.Activity_Start_Date)
+  const values = filteredData.map(edge => edge.node.Result_Value)
 
   return (
-    <Plot
-      data={[
-        {
-          x: dates,
-          y: values,
-          type: 'scatter',
-          mode: 'lines+markers',
-          marker: {color: 'green'}
-        }
-      ]}
-    />
+    <div>
+      <Dropdown
+        placeholder="Select Analyte"
+        fluid
+        selection
+        options={characteristicOptions} //TODO update characteristicName state based on selection 
+      />
+      <Plot
+        data={[
+          {
+            x: dates,
+            y: values,
+            type: "scatter",
+            mode: "lines",
+            marker: { color: "green" },
+          },
+        ]}
+        layout={{
+          title: characteristicName,
+          xaxis: {
+            tickformat: "%Y", // TODO figure out date formatting
+          },
+        }}
+      />
+    </div>
   )
 }
 
