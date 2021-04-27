fcc8ebe3add6ead9e815ef9a29938a2ee4e66230
sadie <sgill@flowwest.com>
2021-04-27 14:42:10 -0700

    adds more to index pg

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
-import { Row, Col, Container } from "react-bootstrap"
-import { Grid } from "semantic-ui-react"
+import { Grid, Card, Header } from "semantic-ui-react"
 import Layout from "../components/layout"
 import HomeBanner from "../components/homeBanner"
 import SEO from "../components/seo"
@@ -27,7 +26,28 @@ const IndexPage = ({ data }) => {
         />
         <Grid container>
           <Grid.Row>
-            <Grid.Column width={6}></Grid.Column>
+            <Grid.Column width={6}>
+              <Grid.Row>
+                <Card href="#">
+                  <Card.Content header="Download Our Data" />
+                  <Img
+                    fluid={imagesObject["beaker"].childImageSharp.fluid}
+                    alt="black line drawing of half full glass beaker"
+                    className="index-icon-image"
+                  />
+                </Card>
+              </Grid.Row>
+              <Grid.Row>
+                <Card href="#">
+                  <Card.Content header="View Water Quality Reports" />
+                  <Img
+                    fluid={imagesObject["report"].childImageSharp.fluid}
+                    alt="black line drawing of report document with pie and bar chart"
+                    className="index-icon-image"
+                  />
+                </Card>
+              </Grid.Row>
+            </Grid.Column>
             <Grid.Column width={10}>
               <p>
                 The monitoring program includes sampling water nutrients, water
@@ -49,12 +69,27 @@ const IndexPage = ({ data }) => {
               </p>
             </Grid.Column>
           </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={6}>
+              <Header as="h3">Sprague River Water Quality Lab (SRWQL)</Header>
+              <p>
+                Since 2006 the Sprague River Water Quality Lab (SRWQL) has
+                allowed the Klamath Tribes to analyze all water nutrients, water
+                chemistry, and algal toxin samples in-house. The lab worked
+                closely with the U.S. Geological Survey’s National Water Quality
+                Lab to complete a laboratory evaluation project comparing the
+                data split between the two labs. The lab has been accredited
+                through the National Environmental Lab Accreditation Program and
+                Oregon Environmental Lab Accreditation Program. The SRWQL
+                utilizes state-of-the-art automated discrete analyzer technology
+                with the Thermo Fisher Aquakem 250. The lab also uses E-friendly
+                methods that employ chemicals that are less toxic to the
+                environment.
+              </p>
+            </Grid.Column>
+            <Grid.Column width={10}></Grid.Column>
+          </Grid.Row>
         </Grid>
-        {/* <Img
-          fluid={imagesObject["beaker"].childImageSharp.fluid}
-          alt="FlowWest Logo"
-          className="index-icon-image"
-        /> */}
       </Layout>
     </div>
   )
