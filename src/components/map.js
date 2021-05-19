937834de4bcdd1d35cfa00924bbe1ed4b4b2964d
Jordan Hoang <jordan.n.hoang@gmail.com>
2021-05-19 08:35:22 -0500

    fixes build issue with leaflet window

--- src/components/map.js
+++ src/components/map.js
@@ -7,11 +7,10 @@ import "leaflet/dist/leaflet.css"
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
 
 export default ({ monitoringLocations }) => {
-  const position = [42.44333343664235, -121.41037747833549]
-
-  let markerIcon = null
-
   if (typeof window !== "undefined") {
+    const position = [42.44333343664235, -121.41037747833549]
+
+    let markerIcon = null
     markerIcon = new L.Icon({
       iconUrl: marker,
       iconRetinaUrl: marker,
@@ -22,38 +21,39 @@ export default ({ monitoringLocations }) => {
       shadowAnchor: null,
       iconSize: new L.Point(20, 35),
     })
+    return (
+      <MapContainer
+        center={position}
+        zoom={9}
+        scrollWheelZoom={false}
+        className="map-container"
+      >
+        <TileLayer
+          attribution="Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community"
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}"
+        />
+        {monitoringLocations.map((location, index) => {
+          var locationName = formatTextCasing(
+            location.node.Monitoring_Location_Name.toLowerCase()
+          )
+
+          return (
+            <Marker
+              position={[location.node.Latitude, location.node.Longitude]}
+              icon={!!markerIcon ? markerIcon : null}
+              key={index}
+            >
+              <Popup>
+                {`Monitoring Location ID: ${location.node.Monitoring_Location_ID}`}
+                <br />
+                {`Monitoring Location Name: ${locationName}`}
+              </Popup>
+            </Marker>
+          )
+        })}
+      </MapContainer>
+    )
+  } else {
+    return <></>
   }
-
-  return (
-    <MapContainer
-      center={position}
-      zoom={9}
-      scrollWheelZoom={false}
-      className="map-container"
-    >
-      <TileLayer
-        attribution="Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community"
-        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}"
-      />
-      {monitoringLocations.map((location, index) => {
-        var locationName = formatTextCasing(
-          location.node.Monitoring_Location_Name.toLowerCase()
-        )
-
-        return (
-          <Marker
-            position={[location.node.Latitude, location.node.Longitude]}
-            icon={!!markerIcon ? markerIcon : null}
-            key={index}
-          >
-            <Popup>
-              {`Monitoring Location ID: ${location.node.Monitoring_Location_ID}`}
-              <br />
-              {`Monitoring Location Name: ${locationName}`}
-            </Popup>
-          </Marker>
-        )
-      })}
-    </MapContainer>
-  )
 }
