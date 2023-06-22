c92911ef1bdb95adeef3f324660f76301ff16115
wdwiii <wdwhitfield3@gmail.com>
2023-06-22 15:51:46 -0400

    Remove negative  margin on stackable grid items

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import { Grid, Header, Dropdown, Search, Card, Button, List } from 'semantic-ui-react'
+import { Grid, Header, Dropdown, Search, Card, Button, List, Divider } from 'semantic-ui-react'
 import { Link } from 'gatsby'
 import WeeklyUploadCard from '../components/WeeklyUploadCard'
 
@@ -22,7 +22,7 @@ const Resources = ({data}) => {
   return (
     <Layout pageInfo={{ pageName: "resources" }}>
        <SEO title="Water Quality Resources" />
-      <Grid container divided='vertically' columns={4} doubling stackable>
+      <Grid container divided='vertically' columns={4} doubling stackable className='remove-negative-margin'>
           <Grid.Row>
             <Header as='h1'>Quick Links</Header>
           </Grid.Row>
@@ -30,13 +30,18 @@ const Resources = ({data}) => {
           <ResourceUpload title='USGS Upper Klamath Lake Elevations' description='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo nobis soluta non sequi doloremque nesciunt expedita minus ipsa. ' link={{to: '#', label: 'Link to USGS Upper Klamath Lake Elevations'}}/>
           <ResourceUpload title='USGS Sprague River Flow and Water Quality' description='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo nobis soluta non sequi doloremque nesciunt expedita minus ipsa. ' link={{to: '#', label: 'Link to USGS Sprague River Flow and Water Quality'}}/>
           <ResourceUpload title='USGS Williamson River Flow and Water Quality' description='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo nobis soluta non sequi doloremque nesciunt expedita minus ipsa. ' link={{to: '#', label: 'Link to USGS Williamson River Flow and Water Quality'}}/>
-       
-        <Grid.Row columns={3}>
-         <WeeklyUploadCard />
-         <WeeklyUploadCard />
-         <WeeklyUploadCard />
-        </Grid.Row>
       </Grid>
+      <Divider section/>
+        <Grid container stackable doubling className='remove-negative-margin'>
+            <Grid.Row>
+              <Header as='h1'>Weekly Reports</Header>
+            </Grid.Row>
+          <Grid.Row columns={3}>
+            <WeeklyUploadCard />
+            <WeeklyUploadCard />
+            <WeeklyUploadCard />
+          </Grid.Row>
+        </Grid>
     </Layout>
   )
 }
