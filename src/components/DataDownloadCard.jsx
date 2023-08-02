42f5db735f605d60cc78be0c1eec380b8968c0f0
wdwiii <wdwhitfield3@gmail.com>
2023-08-02 16:07:54 -0400

    Add callback for toggling report visibility

--- src/components/DataDownloadCard.jsx
+++ src/components/DataDownloadCard.jsx
@@ -1,12 +1,12 @@
-import React, { useContext, useState } from "react"
+import React, { useContext, useState, useCallback } from "react"
 import { Card, Button, Dropdown, Modal } from "semantic-ui-react"
 import { formatTextCasing } from "../helpers/utils"
 import { UserContext } from "../../gatsby-browser"
 import UploadReportForm from "./UploadReportForm"
+import * as AWS from "aws-sdk"
 
 const DataDownloadCard = ({ reportMetaData, allReports, getAllReports }) => {
   const [editReportModalOpen, setEditReportModalOpen] = useState(false)
-  const [reportIsHidden, setReportIsHidden] = useState(false)
   const reportIsActive = reportMetaData.active === "TRUE"
   const { user } = useContext(UserContext)
   const authorsArray = reportMetaData.authors.split(",")
@@ -22,20 +22,39 @@ const DataDownloadCard = ({ reportMetaData, allReports, getAllReports }) => {
     }`
   }
 
+  const handleVisibilityToggle = useCallback(async () => {
+    AWS.config.update({ region: "us-west-1" })
+    const docClient = new AWS.DynamoDB.DocumentClient()
+    const tableName = "reports_metadata"
+
+    try {
+      const params = {
+        TableName: tableName,
+        Key: { report_uuid: reportMetaData.report_uuid },
+        UpdateExpression: "set #active = :active",
+        ExpressionAttributeNames: {
+          "#active": "active",
+        },
+        ExpressionAttributeValues: {
+          ":active": reportIsActive ? "FALSE" : "TRUE",
+        },
+      }
+      await docClient.update(params).promise()
+    } catch (error) {
+      console.log("🚀 ~ handleVisibilityToggle ~ error:", error)
+    } finally {
+      await getAllReports()
+    }
+  }, [reportIsActive])
+
   return (
     <>
       <Card
         color="blue"
         link
         fluid
-        className={`report-card ${!reportIsHidden ? "" : "report-card-hidden"}`}
-      >
-        {/* <Card
-        color="blue"
-        link
-        fluid
         className={`report-card ${reportIsActive ? "" : "report-card-hidden"}`}
-      > */}
+      >
         <Card.Content>
           <Card.Header as="h6" className="report-card-header">
             {reportMetaData.title}
@@ -93,14 +112,14 @@ const DataDownloadCard = ({ reportMetaData, allReports, getAllReports }) => {
                       size="tiny"
                       trigger={
                         <Dropdown.Item>
-                          Mark as {!reportIsHidden ? "Hidden" : "Visible"}
+                          Mark as {reportIsActive ? "Hidden" : "Visible"}
                         </Dropdown.Item>
                       }
                       header={`Toggle Report Visibility (${
-                        !reportIsHidden ? "Hidden" : "Visible"
+                        reportIsActive ? "Hidden" : "Visible"
                       })`}
                       content={`${
-                        !reportIsHidden
+                        reportIsActive
                           ? "Setting the report visibility status to hidden will only allow users with administrator access to view the content of the report."
                           : "Setting the report visibility status to visible will allow visitors to view the content of the report."
                       } Do you wish to proceed?`}
@@ -109,10 +128,9 @@ const DataDownloadCard = ({ reportMetaData, allReports, getAllReports }) => {
                         {
                           key: "proceed",
                           content: "Proceed",
-                          negative: !reportIsHidden,
-                          positive: reportIsHidden,
-                          onClick: () =>
-                            setReportIsHidden((prevState) => !prevState),
+                          negative: reportIsActive,
+                          positive: !reportIsActive,
+                          onClick: () => handleVisibilityToggle(),
                         },
                       ]}
                     />
