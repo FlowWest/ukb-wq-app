0a377769e48a1ef62c346fb8365d65be2b4f6382
Jordan Hoang <jordan.n.hoang@gmail.com>
2023-08-14 11:39:08 -0500

    comments out eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,29 +43,29 @@ module.exports = {
         path: `${__dirname}/src/data/`,
       },
     },
-    {
-      resolve: "gatsby-plugin-prettier-eslint",
-      options: {
-        prettier: {
-          patterns: [
-            // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
-            "**/*.{css,scss,less}",
-            "**/*.{json,json5}",
-            "**/*.{graphql}",
-            "**/*.{md,mdx}",
-            "**/*.{html}",
-            "**/*.{yaml,yml}",
-          ],
-        },
-        eslint: {
-          patterns: "**/*.{js,jsx,ts,tsx}",
-          customOptions: {
-            fix: true,
-            cache: true,
-          },
-        },
-      },
-    },
+    // {
+    //   resolve: "gatsby-plugin-prettier-eslint",
+    //   options: {
+    //     prettier: {
+    //       patterns: [
+    //         // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
+    //         "**/*.{css,scss,less}",
+    //         "**/*.{json,json5}",
+    //         "**/*.{graphql}",
+    //         "**/*.{md,mdx}",
+    //         "**/*.{html}",
+    //         "**/*.{yaml,yml}",
+    //       ],
+    //     },
+    //     eslint: {
+    //       patterns: "**/*.{js,jsx,ts,tsx}",
+    //       customOptions: {
+    //         fix: true,
+    //         cache: true,
+    //       },
+    //     },
+    //   },
+    // },
     `gatsby-transformer-csv`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
