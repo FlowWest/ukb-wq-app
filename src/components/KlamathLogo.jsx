05c36b2844a9823a2765a9a003971546c2ef74d9
wdwiii <wdwhitfield3@gmail.com>
2023-11-01 15:05:30 -0400

    Configure logo to accept width prop

--- src/components/KlamathLogo.jsx
+++ src/components/KlamathLogo.jsx
@@ -2,34 +2,41 @@ import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
-const KlamathLogo = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        file(relativePath: { eq: "klamathtribes1200.png" }) {
-          childImageSharp {
-            fixed(width: 500) {
-              ...GatsbyImageSharpFixed_noBase64
-            }
-            fluid(maxWidth: 1200) {
-              # Choose either the fragment including a small base64ed image, a traced placeholder SVG, or one without.
-              ...GatsbyImageSharpFluid
-              ...GatsbyImageSharpFluidLimitPresentationSize
+const KlamathLogo = ({ mobile }) => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          file(relativePath: { eq: "klamathtribes1200.png" }) {
+            childImageSharp {
+              fixed(width: 500) {
+                ...GatsbyImageSharpFixed_noBase64
+              }
+              fluid(maxWidth: 1200) {
+                # Choose either the fragment including a small base64ed image, a traced placeholder SVG, or one without.
+                ...GatsbyImageSharpFluid
+                ...GatsbyImageSharpFluidLimitPresentationSize
+              }
             }
           }
         }
-      }
-    `}
-    render={(data) => (
-      <div style={{ width: 400 }}>
-        <Img
-          //fixed={data.file.childImageSharp.fixed}
-          fluid={data.file.childImageSharp.fluid}
-          alt="Klamath Logo"
-        />
-      </div>
-    )}
-  />
-)
+      `}
+      render={(data) => (
+        <div
+          style={{
+            width: mobile ? 250 : 400,
+            margin: `${mobile ? "auto" : 0}`,
+          }}
+        >
+          <Img
+            //fixed={data.file.childImageSharp.fixed}
+            fluid={data.file.childImageSharp.fluid}
+            alt="Klamath Logo"
+          />
+        </div>
+      )}
+    />
+  )
+}
 
 export default KlamathLogo
